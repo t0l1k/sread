a05172a68bb6766abd07cca1a9935a00a8738cd2
Anatoli Tjushka <t0l1k@mail.ru>
2024-01-23 16:22:08 +0200

    storing the database in the location specified by the running OS

--- app/data/db.go
+++ app/data/db.go
@@ -1,9 +1,11 @@
 package data
 
 import (
-	"os"
+	"fmt"
+	"strings"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/quasilyte/gdata"
 
 	"database/sql"
 	"log"
@@ -29,30 +31,42 @@ func GetDb() (db *Db) {
 }
 
 func (d *Db) Setup() {
-	// check dir present
-	if _, err := os.Stat("texts"); os.IsNotExist(err) {
-		err := os.Mkdir("texts", os.ModePerm)
-		if err != nil {
-			panic(err)
-		}
-	}
 	d.createBooksTable()
 }
 
 func (d *Db) createBooksTable() {
 	var err error
-	d.conn, err = sql.Open("sqlite3", "texts/books.db")
+	filename := d.getDbPath()
+	d.conn, err = sql.Open("sqlite3", filename)
 	if err != nil {
+		log.Println(err)
 		panic(err)
 	}
 	var createGameDB string = "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, dt TEXT, count INTEGER, idx INTEGER, lastspeed INTEGER, status INTEGER, content TEXT)"
 	cur, err := d.conn.Prepare(createGameDB)
 	if err != nil {
+		log.Println(err)
 		panic(err)
 	}
 	cur.Exec()
 	cur.Close()
 	log.Println("Created table for books.")
+}
+
+func (*Db) getDbPath() string {
+	m, err := gdata.Open(gdata.Config{AppName: "sread"})
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
+	s := []byte("spritz reader")
+	if err := m.SaveItem("sread.txt", s); err != nil {
+		log.Println(err)
+		panic(err)
+	}
+	filename := fmt.Sprintf("%v/books.db", strings.TrimSuffix(m.ItemPath("sread.txt"), "/sread.txt"))
+	log.Println("to db path", strings.TrimSuffix(m.ItemPath("sread.txt"), "/sread.txt"), filename)
+	return filename
 }
 
 func (d *Db) InsertBook(values *Book) {
@@ -99,6 +113,7 @@ func (d *Db) GetFromDbHistory() *History {
 	}
 	rows, err := d.conn.Query("SELECT * FROM books")
 	if err != nil {
+		log.Println(err)
 		panic(err)
 	}
 	defer rows.Close()
@@ -108,6 +123,7 @@ func (d *Db) GetFromDbHistory() *History {
 		id := 0
 		err = rows.Scan(&id, &txt.name, &txt.dt, &txt.count, &txt.idx, &txt.lastSpeed, &txt.status, &txt.content)
 		if err != nil && err != sql.ErrNoRows {
+			log.Println(err)
 			panic(err)
 		}
 		history.New(id, txt)
@@ -122,6 +138,7 @@ func (d *Db) GetNames() []string {
 	}
 	rows, err := d.conn.Query("SELECT * FROM books")
 	if err != nil {
+		log.Println(err)
 		panic(err)
 	}
 	defer rows.Close()
@@ -131,6 +148,7 @@ func (d *Db) GetNames() []string {
 		id := 0
 		err = rows.Scan(&id, &book.name, &book.dt, &book.count, &book.idx, &book.lastSpeed, &book.status, &book.content)
 		if err != nil && err != sql.ErrNoRows {
+			log.Println(err)
 			panic(err)
 		}
 		names = append(names, book.name)
@@ -157,6 +175,7 @@ func (d *Db) DeleteRecords(names []string) {
 	}
 	count, err := res.RowsAffected()
 	if err != nil {
+		log.Println(err)
 		panic(err)
 	}
 	log.Println("DB DELETE AFFECTED:", count)
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/hajimehoshi/ebiten/v2 v2.6.2
 	github.com/mattn/go-sqlite3 v1.14.18
+	github.com/quasilyte/gdata v0.8.1
 	github.com/t0l1k/eui v0.0.0-20240121190708-129b31cfc8d3
 	golang.design/x/clipboard v0.7.0
 	golang.org/x/image v0.14.0
@@ -13,7 +14,6 @@ require (
 require (
 	github.com/ebitengine/purego v0.5.0 // indirect
 	github.com/jezek/xgb v1.1.0 // indirect
-	golang.org/x/exp/shiny v0.0.0-20231110203233-9a3e6036ecaa // indirect
 	golang.org/x/mobile v0.0.0-20231108233038-35478a0c49da // indirect
 	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
